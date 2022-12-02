318d7894a8c91131d853126ff68264416ab09cd7
Klimenty Karavaev <klimentykk@gmail.com>
2022-12-02 23:55:02 +0400

    Add todo create functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from "react";
 //Components
 import TodoForm from "./Components/TodoForm";
 import Heading from "./Components/Heading";
-import Counter from "../src/Components/Counter";
-import CounterStore from "./Store/CounterStore";
+import TodoList from "./Components/TodoList";
+
 //CSS
 import styles from "./Styles/App.module.css";
+
+//Store
+import TodoStore from "./Store/TodoStore";
 
 // TODO:
 // 1. Make a todo list component sortable, filterable and rearrangeable with drag and drop API;
@@ -17,7 +20,8 @@ function App() {
     <div className="App">
       <main className={styles.mainContainer}>
         <Heading />
-        <TodoForm />
+        <TodoForm store={TodoStore} />
+        <TodoList store={TodoStore} />
       </main>
     </div>
   );
--- src/Components/TodoForm.tsx
+++ src/Components/TodoForm.tsx
@@ -1,35 +1,67 @@
-import React from "react";
-import { Importance } from "../types/TodoTypes";
+import React, { useState } from "react";
 import styles from "../Styles/TodoForm.module.css";
 import { v4 } from "uuid";
+import { TodoStoreType } from "../types/TodoTypes";
 
 const optionsArray = (() => {
   const options: Array<React.ReactElement> = [];
-  const keys = Object.keys(Importance).filter((el) => !/\d+/.test(el));
+  const names = ["Important", "Can wait", "Unimportant"];
 
-  for (let item of keys) {
-    options.push(<option key={v4()}>{item}</option>);
+  for (let i = 0; i < names.length; i++) {
+    options.push(<option key={v4()}>{names[i]}</option>);
   }
 
   return options;
 })();
 
-const TodoForm: React.FC<{}> = () => {
+interface Props {
+  store: TodoStoreType;
+}
+
+const TodoForm: React.FC<Props> = ({ store }) => {
+  const [title, setTitle] = useState<string>("");
+  const [info, setInfo] = useState<string>("");
+
+  const titleHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("yay");
+    setTitle(e.target.value);
+  };
+
+  const infoHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInfo(e.target.value);
+  };
+
   return (
     <form>
       <fieldset className={styles.fieldSetContainer}>
         <legend>Create a Todo</legend>
 
         <label htmlFor="todo-title">Title</label>
-        <input type="text" name="title" id="todo-title" />
+        <input
+          onChange={titleHandler}
+          type="text"
+          name="title"
+          id="todo-title"
+        />
 
-        <label htmlFor="date">Green</label>
-        <input type="date" name="completion-date" id="date" />
+        <label htmlFor="date">Description</label>
+        <input
+          onChange={infoHandler}
+          type="text"
+          name="completion-date"
+          id="date"
+        />
 
-        <label htmlFor="colour_blue">Importance</label>
+        <label htmlFor="">Importance</label>
         <select>
           <optgroup>{optionsArray}</optgroup>
         </select>
+
+        <input
+          onClick={() => store.addTodo(title, info)}
+          type="button"
+          value="Create a new todo"
+        />
       </fieldset>
     </form>
   );
